79697b6c4ca28e89dc3d1fc929d399c7ead272f0
SuhailG <zonyc17@msn.com>
2018-10-22 16:44:19 -0600

    Add error messages to top bar if loading issues occur

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Menu, Icon, Label, Popup, Search } from 'semantic-ui-react'
+import { Menu, Label, Popup, Search } from 'semantic-ui-react'
 import ConnectionStatus from './ConnectionStatus'
 
 export default class TopMenu extends Component {
@@ -50,7 +50,7 @@ export default class TopMenu extends Component {
                     return cdp.title === `ID: ${value}`
                 })
                 this.setState({isLoading: false, results})
-            }, 300)
+            }, 400)
     }
 
 
@@ -67,8 +67,9 @@ export default class TopMenu extends Component {
                         <polygon points="9.571,20.464 17.635,24.934 25.7,20.464 25.7,11.525 17.635,7.056 9.571,11.525 " style={{fill:"#B8BCBF"}} /> */}
                         <path d="M0,16.024L15.976,32L32,15.976L16.024,0L0,16.024z M15.989,23.347l-7.336-7.336l14.694-0.022L15.989,23.347z" style={{fill:"#B8BCBF"}}></path>
                     </svg><span style={{fontFamily:"Roboto", fontWeight:500}}>Dai Embassy</span>
-                <Popup inverted position='right center' trigger={<Label color='red'>Alpha</Label>} content='Alpha 0.0.1'/></Menu.Item>
-                <Menu.Item fluid inverted><Search fluid value={this.state.value} loading={this.state.isLoading} onSearchChange={this.handleSearchChange} onResultSelect={this.props.handleSearchClick} results = {this.state.results} size='mini'/></Menu.Item>
+                <Popup inverted position='right center' trigger={<Label color='red'>Alpha</Label>} content='Alpha 0.1.0'/></Menu.Item>
+                <Menu.Item fluid inverted><Search fluid selectFirstResult value={this.state.value} loading={this.state.isLoading} onSearchChange={this.handleSearchChange} onResultSelect={this.props.handleSearchClick} results = {this.state.results} size='mini'/></Menu.Item>
+                {this.props.account ? <Menu.Item><p style={{color:'#FF695E'}}>{this.props.loadingMsg}</p></Menu.Item> : null}
                 {/* <Menu.Item name = 'CDP' color='blue' active={activeItem === 'CDP'} onClick={this.handleClick}/>
                 <Menu.Item name = 'Exchange' color='blue' active={activeItem === 'Exchange'} onClick={this.handleClick}/>
                 <Menu.Item name = 'Send' color='blue' active={activeItem === 'Send'} onClick={this.handleClick}/> */}
