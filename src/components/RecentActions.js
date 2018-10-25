9f413f595e763df2ee571395701525afa9d0ad30
SuhailG <zonyc17@msn.com>
2018-10-25 03:41:06 -0600

    Improve table look and feel. Also added dynamic data

--- src/components/RecentActions.js
+++ src/components/RecentActions.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react'
-import { Card, Feed } from 'semantic-ui-react'
+import { Table, Segment} from 'semantic-ui-react'
 
 class RecentActions extends Component{
-    state = {}
+    state = { 
+        cdp: this.props.cdps.find((cdp) =>{
+        return cdp.cdpId === this.props.cdpId
+    })}
+
+    componentDidMount(){
+        let actions = this.state.cdp.actions
+        console.log('actions: ', actions)
+        // console.log('Recent Actions State: ',this.state)
+    }
+
+    getActions = () =>{
+        let recentActions = []
+        const actions = this.state.cdp.actions
+        console.log('actions: ', actions)
+        actions.forEach(action => {
+            recentActions.push(<Table.Row>
+                <Table.Cell>
+                    {action.act}
+                </Table.Cell>
+                <Table.Cell singleLine>
+                    {new Date(action.time).toString().slice(0,-37)}
+                </Table.Cell>
+                <Table.Cell>
+                    {action.act == 'DRAW' || action.act == 'WIPE' ? `${parseFloat(action.arg).toFixed(2)} DAI` : `${parseFloat(action.arg).toFixed(2)} PETH`}
+                </Table.Cell>
+                <Table.Cell>
+                    <a target="_blank" href={`https://etherscan.io/tx/${action.tx}`} style={{textDecoration:'underline', color:'inherit'}}>{this.truncateTx(action.tx)}</a>
+                </Table.Cell>
+            </Table.Row>)
+        })
+        return recentActions
+    }
+
+    truncateTx = (tx) => {
+        return `${tx.slice(0,6)}...${tx.slice(-6)}` 
+    }
 
     render(){
         return(
-            <div style={{paddingTop:'10px'}}>
-                <Card fluid>
-                    <Card.Content header="Recent Actions"/>
-                    <Card.Content>
-                    <Feed><Feed.Event date='39 min ago' summary='Open CDP'/></Feed>
-                    <Feed><Feed.Event date='24 min ago' summary='Lock'/></Feed>
-                    <Feed><Feed.Event date='22 min ago' summary='Draw'/></Feed>
-                    </Card.Content>
-                </Card>
-            </div>
+            <Segment style={{paddingTop:'10px', backgroundColor:'#232D39', borderRadius:'5px', border: '2px solid #38414B',maxHeight:364, overflow:'auto'}}>
+            <Table inverted compact style={{backgroundColor:'#3D4853',}}>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Action</Table.HeaderCell>
+                        <Table.HeaderCell>Date</Table.HeaderCell>
+                        <Table.HeaderCell>Quantity</Table.HeaderCell>
+                        <Table.HeaderCell>Tx Hash</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body style={{backgroundColor:'#273340'}}>
+                    {this.getActions()}
+                </Table.Body>
+            </Table>
+            </Segment>
         )
     }
 }
