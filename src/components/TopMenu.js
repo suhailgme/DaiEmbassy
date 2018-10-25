3667c7643df6b1f3c8d9a4cad7c23a2dc4658b3a
SuhailG <zonyc17@msn.com>
2018-10-25 03:41:32 -0600

    Tweak menu to show search only when cdps are loaded from remote

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -62,16 +62,13 @@ export default class TopMenu extends Component {
             <Menu fluid inverted stackable borderless attached size='huge' style={{marginBottom:'5px', backgroundColor:'#3D4853'}}>
                 <Menu.Item style={{opacity:'0.8'}}> 
                     <svg height="35" width="35">
-                        {/* <polygon points="32,0 0,0 0,32 32,32 26.946,26.828 5.054,26.828 5.054,5.172 26.946,5.172 " style={{fill:"#B8BCBF"}} />
-                        <polygon points="9.571,20.464 17.635,24.934 25.7,20.464 25.7,11.525 17.635,7.056 9.571,11.525 " style={{fill:"#B8BCBF"}} /> */}
+                        {/* <polygon points="32,0 0,0 0,32 32,32 26.946,26.828 5.054,26.828 5.054,5.172 26.946,5.172 " style={{fill:"#B8BCBF"}} /> */}
+                        {/* <polygon points="9.571,20.464 17.635,24.934 25.7,20.464 25.7,11.525 17.635,7.056 9.571,11.525 " style={{fill:"#B8BCBF"}} /> */}
                         <path d="M0,16.024L15.976,32L32,15.976L16.024,0L0,16.024z M15.989,23.347l-7.336-7.336l14.694-0.022L15.989,23.347z" style={{fill:"#B8BCBF"}}></path>
                     </svg><span style={{fontFamily:"Roboto", fontWeight:500}}>Dai Embassy</span>
                 <Popup inverted position='right center' trigger={<Label color='red'>Alpha</Label>} content='Alpha 0.1.0'/></Menu.Item>
-                <Menu.Item fluid inverted><Search fluid selectFirstResult value={this.state.value} loading={this.state.isLoading} onSearchChange={this.handleSearchChange} onResultSelect={this.props.handleSearchClick} results = {this.state.results} size='mini'/></Menu.Item>
-                {this.props.account ? <Menu.Item><p style={{color:'#FF695E'}}>{this.props.loadingMsg}</p></Menu.Item> : null}
-                {/* <Menu.Item name = 'CDP' color='blue' active={activeItem === 'CDP'} onClick={this.handleClick}/>
-                <Menu.Item name = 'Exchange' color='blue' active={activeItem === 'Exchange'} onClick={this.handleClick}/>
-                <Menu.Item name = 'Send' color='blue' active={activeItem === 'Send'} onClick={this.handleClick}/> */}
+                {this.props.cdps ? <Menu.Item fluid inverted><Search fluid selectFirstResult value={this.state.value} loading={this.state.isLoading} onSearchChange={this.handleSearchChange} onResultSelect={this.props.handleSearchClick} results = {this.state.results} size='mini'/></Menu.Item> : null}
+                {this.props.searchMsg ? <Menu.Item><p style={{color:'#FF695E'}}>{this.props.searchMsg}</p></Menu.Item> : null}
                 <Menu.Item position='right'><ConnectionStatus loadingMsg = {this.props.loadingMsg} account={this.props.account}/> </Menu.Item> 
 
             </Menu>
