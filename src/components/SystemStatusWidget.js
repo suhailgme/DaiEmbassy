286df13f6e94c315276fc208bbc42b8f6a6aa896
SuhailG <zonyc17@msn.com>
2018-11-23 03:08:37 -0700

    Update system status widget
    - Change Dai to Circ Dai for clarity
    - Changed Peth to locked peth for clarity
    - Add total cdps count to component

--- src/components/SystemStatusWidget.js
+++ src/components/SystemStatusWidget.js
@@ -12,13 +12,17 @@ export default class SystemStatusWidget extends Component{
     numberWithCommas = (circulatingDai) => {
         return circulatingDai.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
       }
+    
+    calculateLockedPeth = () =>{
+        return ((this.state.circulatingDai*(this.state.systemCollateralization/100) / this.state.ethPrice) / this.state.pethWethRatio)
+    }
 
 
     render(){
         return (
             <div>
                 <Container textAlign='center'>
-                    <Statistic size='mini' color='red' inverted label='ETH' value={this.state.ethPrice}/>
+                    <Statistic size='mini' color='red' inverted label='ETH/USD' value={this.state.ethPrice}/>
                 </Container>
                 <Grid columns='equal' padded='vertically'>
                     <Grid.Row style={{paddingBottom:'5px'}}>
@@ -34,16 +38,20 @@ export default class SystemStatusWidget extends Component{
                         <Grid.Column textAlign='right'>{this.state.pethWethRatio}</Grid.Column>
                     </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>DAI</Grid.Column>
+                        <Grid.Column textAlign='left'>Circ. DAI</Grid.Column>
                         <Grid.Column textAlign='right' style={{color:'#FF695E', whiteSpace:'nowrap'}}>{`${this.numberWithCommas(this.state.circulatingDai)}`}</Grid.Column>
                     </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>PETH</Grid.Column>
-                        <Grid.Column textAlign='right' style={{color:'#FF695E', whiteSpace:'nowrap'}}>{`${this.numberWithCommas(this.state.circulatingDai*(this.state.systemCollateralization/100))}`}</Grid.Column>
+                        <Grid.Column textAlign='left'>Locked PETH</Grid.Column>
+                        <Grid.Column textAlign='right' style={{color:'#FF695E', whiteSpace:'nowrap'}}>{`${this.numberWithCommas(this.calculateLockedPeth())}`}</Grid.Column>
                     </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
                         <Grid.Column textAlign='left'>Collateralization</Grid.Column>
                         <Grid.Column textAlign='right' style={{color:'#6abf69'}}>{this.state.systemCollateralization}%</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
+                        <Grid.Column textAlign='left'>Total CDPs</Grid.Column>
+                        <Grid.Column textAlign='right'>{this.state.totalCDPs}</Grid.Column>
                     </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
                         <Grid.Column textAlign='left'>Updated</Grid.Column>
