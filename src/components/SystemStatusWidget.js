dba4feaac78e6f694938e27afd4f40d3bdc7713c
SuhailG <zonyc17@msn.com>
2018-12-28 05:02:05 -0700

    Add avg debt, avg collateral, eth supply locked
    - average debt of all open CDPS is now shown
    - average collateral of all open CDPS is now shown
    - Percent of eth supply locked is now shown

--- src/components/SystemStatusWidget.js
+++ src/components/SystemStatusWidget.js
@@ -1,40 +1,88 @@
 import React, { Component } from 'react'
-import { Grid, Statistic, Container } from 'semantic-ui-react'
+import { Grid, Statistic, Container, Form } from 'semantic-ui-react'
 
 export default class SystemStatusWidget extends Component{
     
     state = {...this.props.systemStatus}
 
     componentDidMount(){
+        // this.setState({...this.props.systemStatus})
         // console.log('systemStatus: ', this.state)
+        // console.log(this.state)
     }
 
-    numberWithCommas = (circulatingDai) => {
-        return circulatingDai.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    static getDerivedStateFromProps(nextProps, prevState){
+        // console.log('system status: ', nextProps.systemStatus)
+        return {...nextProps.systemStatus}
+      }
+
+    numberWithCommas = (number) => {
+        return number.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
       }
     
     calculateLockedPeth = () =>{
         return ((this.state.circulatingDai*(this.state.systemCollateralization/100) / this.state.ethPrice) / this.state.pethWethRatio)
     }
 
+ 
+
+
+    calculateAvgDebt = () =>{
+        const cdps = this.state.cdps
+        const sum = cdps.reduce((acc,cdp) =>{
+            return acc + parseFloat(cdp.daiDebt)
+        },0)
+        const avgDebt = sum/cdps.length
+        // console.log("avgDebt: ", avgDebt)
+        return this.numberWithCommas(avgDebt)
+    }
+
+    calculateAvgCollateral = () =>{
+        const cdps = this.state.cdps
+        const sum = cdps.reduce((acc,cdp) =>{
+            return acc + parseFloat(cdp.pethCollateral)
+        },0)
+        const avgCollateral = sum/cdps.length
+        // console.log("avgCollateral: ", avgCollateral)
+        return this.numberWithCommas(avgCollateral)
+    }
+
+    calculateEthSupplyLocked = () =>{
+        const petWeth = this.state.pethWethRatio
+        const ethSupply = this.state.ethSupply
+        const lockedPeth = this.calculateLockedPeth()
+        const lockedEthSupply = parseFloat((((lockedPeth * (petWeth)) / ethSupply) * 100)).toFixed(3)
+        // console.log('lockedEthSupply: ', lockedEthSupply)
+        return lockedEthSupply
+
+        
+    }
 
     render(){
+        const avgDebt = this.calculateAvgDebt()
+        const avgCollateral = this.calculateAvgCollateral()
+        const ethSupplyLocked = this.calculateEthSupplyLocked()
         return (
             <div>
                 <Container textAlign='center'>
                     <Statistic size='mini' color='red' inverted label='ETH/USD' value={this.state.ethPrice}/>
                 </Container>
+                <Form>
                 <Grid columns='equal' padded='vertically'>
-                    <Grid.Row style={{paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>Eth Price</Grid.Column>
-                        <Grid.Column textAlign='right' style={{color:'#FF695E'}}>{this.state.ethPrice} USD</Grid.Column>
-                    </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>Mkr Price</Grid.Column>
+                        <Grid.Column textAlign='left'>MKR Price</Grid.Column>
                         <Grid.Column textAlign='right' style={{color:'#6abf69'}}>{this.state.mkrPrice} USD</Grid.Column>
                     </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>PETH/WETH</Grid.Column>
+                        <Grid.Column textAlign='left' style={{whiteSpace:'nowrap' }}>ETH Supply Locked</Grid.Column>
+                        <Grid.Column textAlign='right' style={{color:'#6abf69'}}>{ethSupplyLocked} %</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
+                        <Grid.Column textAlign='left'>Locked PETH</Grid.Column>
+                        <Grid.Column textAlign='right' style={{color:'#FF695E', whiteSpace:'nowrap'}}>{`${this.numberWithCommas(this.calculateLockedPeth())}`}</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
+                        <Grid.Column textAlign='left'>PETH/ETH</Grid.Column>
                         <Grid.Column textAlign='right'>{this.state.pethWethRatio}</Grid.Column>
                     </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
@@ -42,8 +90,12 @@ export default class SystemStatusWidget extends Component{
                         <Grid.Column textAlign='right' style={{color:'#FF695E', whiteSpace:'nowrap'}}>{`${this.numberWithCommas(this.state.circulatingDai)}`}</Grid.Column>
                     </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>Locked PETH</Grid.Column>
-                        <Grid.Column textAlign='right' style={{color:'#FF695E', whiteSpace:'nowrap'}}>{`${this.numberWithCommas(this.calculateLockedPeth())}`}</Grid.Column>
+                        <Grid.Column textAlign='left' style={{whiteSpace:'nowrap'}}>Avg. CDP Debt</Grid.Column>
+                        <Grid.Column textAlign='right' style={{color:'#6abf69'}}>{avgDebt} DAI</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
+                        <Grid.Column textAlign='left' style={{whiteSpace:'nowrap' }}>Avg. CDP Collateral</Grid.Column>
+                        <Grid.Column textAlign='right' style={{color:'#6abf69'}}>{avgCollateral} PETH</Grid.Column>
                     </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
                         <Grid.Column textAlign='left'>Collateralization</Grid.Column>
@@ -55,9 +107,10 @@ export default class SystemStatusWidget extends Component{
                     </Grid.Row>
                     <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
                         <Grid.Column textAlign='left'>Updated</Grid.Column>
-                        <Grid.Column textAlign='right'>{Date().toString().slice(16,-37)}</Grid.Column>
+                        <Grid.Column textAlign='right'>{Date().toString().slice(4,-47) + Date().slice(16,-37)}</Grid.Column>
                     </Grid.Row>
                 </Grid>
+                </Form>
             </div>
         )
     }
