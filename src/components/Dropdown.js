1c960049c70da1e178e4624ec78f5e2d738668b7
SuhailG <zonyc17@msn.com>
2018-12-28 05:01:04 -0700

    Added handling of loading members of the side menu
    - loader icons are displayed when query is loading and on initial load

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Accordion, Loader } from 'semantic-ui-react'
 import WipeDrawWidget from './WipeDrawWidget'
 import CdpDetails from './CdpDetails'
+import StatsWidget from './StatsWidget'
 import SystemStatusWidget from './SystemStatusWidget'
 
 
@@ -9,17 +10,17 @@ import SystemStatusWidget from './SystemStatusWidget'
 const WidgetAccordion = (props) => <Accordion defaultActiveIndex={[0,1,2]} panels={[
   {
     key: '0',
-    title: `Manage CDP ${props.cdpId}`,
+    title: {content: props.wipeDraw ? `Simulate CDP ${props.cdpId}` : `Loading CDP`},
     content: {content: props.wipeDraw ? (<WipeDrawWidget wipeDraw={props.wipeDraw}/>) : <Loader active inverted inline='centered'/>}
   },
   {
     key: '1',
-    title: `Details CDP ${props.cdpId}`,
+    title: {content: props.cdpDetails ? `Details CDP ${props.cdpId}` : `Loading CDP Details`},
     content: {content: props.cdpDetails ? (<CdpDetails cdpDetails={props.cdpDetails}/>) : <Loader active inverted inline='centered'/>},
   },
   {
     key: '2',
-    title: 'System Status',
+    title: {content: props.systemStatus ? `Maker System Statistics` : `Loading System Statistics`},
     content: {content: props.systemStatus ? (<SystemStatusWidget systemStatus={props.systemStatus} />) : <Loader active inverted inline='centered'/>},
   }
 ]} exclusive={false} inverted />
