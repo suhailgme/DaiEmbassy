53e409fd713aa3734b6650cf9a1c073f58364eca
SuhailG <zonyc17@msn.com>
2019-05-05 21:45:07 -0600

    Cumulative DAI/PETH now changes based on number of rows selected
    - Add icon to address to make it more clear that they are external links

--- src/components/AllCdps.js
+++ src/components/AllCdps.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactTable from "react-table";
 import "react-table/react-table.css";
-import { Loader, Icon } from 'semantic-ui-react'
+import { Loader, Icon, Grid } from 'semantic-ui-react'
 const Humanize = require('humanize-plus')
 
 
@@ -11,6 +11,7 @@ export default class AllCdps extends Component{
         cdps: this.props.cdps,
         filtered: true,
         minDebt: "0.01",
+        pageSize: 10
     }
 
 
@@ -52,7 +53,7 @@ export default class AllCdps extends Component{
         this.props.handleSearchClick(null, {value: e.target.value})
     }
 
-    processCdps = () =>{
+    processCdps = (pageSize = 10) =>{
         const cdps = this.state.cdps
         let top100Debt = 0 
         let top100Collateral = 0
@@ -62,7 +63,7 @@ export default class AllCdps extends Component{
             if(this.numberWithCommas(cdp.daiDebt) >= this.state.minDebt)
             processedCdps.push({
                 cdpId: <button style={{background: 'none', border:'none', padding:0, textDecoration:'underline', color:'#FFF', cursor:'pointer'}} value={cdp.cdpId} onClick={this.handleClick}>{cdp.cdpId}</button>,
-                account: <a target="_blank" href={`https://etherscan.io/address/${cdp.account}`} style={{textDecoration:'underline', color:'inherit'}}>{cdp.account}</a>,
+                account: <span><Icon name='external'/> <a target="_blank" href={`https://etherscan.io/address/${cdp.account}`} style={{textDecoration:'underline', color:'inherit'}}>{cdp.account}</a></span>,
                 daiDebt: this.numberWithCommas(cdp.daiDebt),
                 pethCollateral: this.numberWithCommas(cdp.pethCollateral),
                 usdCollateral: this.numberWithCommas(cdp.usdCollateral),
@@ -71,7 +72,7 @@ export default class AllCdps extends Component{
         }else{
             processedCdps.push({
                 cdpId: <button style={{background: 'none', border:'none', padding:0, textDecoration:'underline', color:'#FFF', cursor:'pointer'}} value={cdp.cdpId} onClick={this.handleClick}>{cdp.cdpId}</button>,
-                account: <a target="_blank" href={`https://etherscan.io/address/${cdp.account}`} style={{textDecoration:'underline', color:'inherit'}}>{cdp.account}</a>,
+                account: <span><Icon name='external'/> <a target="_blank" href={`https://etherscan.io/address/${cdp.account}`} style={{textDecoration:'underline', color:'inherit'}}>{cdp.account}</a></span>,
                 daiDebt: this.numberWithCommas(cdp.daiDebt),
                 pethCollateral: this.numberWithCommas(cdp.pethCollateral),
                 usdCollateral: this.numberWithCommas(cdp.usdCollateral),
@@ -85,7 +86,7 @@ export default class AllCdps extends Component{
             return (b.daiDebt) - (a.daiDebt)
         })
         cdps.forEach((cdp, index) =>{
-            if(index < 100){
+            if(index < pageSize){
                 top100Debt += +cdp.daiDebt
             }
         })
@@ -93,13 +94,13 @@ export default class AllCdps extends Component{
             return (b.pethCollateral) - (a.pethCollateral)
         })
         cdps.forEach((cdp, index) =>{
-            if(index < 100){
+            if(index < pageSize){
                 top100Collateral += +cdp.pethCollateral
             }
         })
 
 
-        this.setState({processedCdps, numberCDPs: processedCdps.length, top100Debt, top100Collateral})
+        this.setState({processedCdps, numberCDPs: processedCdps.length, top100Debt, top100Collateral, pageSize})
     }
 
     filterTable = () =>{
@@ -117,9 +118,18 @@ export default class AllCdps extends Component{
         return (
             <div style={{color:'#FFF', borderRadius:'5px', border: '2px solid #38414B',
             backgroundColor:'#273340', paddingTop:'10px', paddingLeft:'5px'}}>
-                <h4 ><Icon name='globe'/>Open CDPs (Total {numberCDPs})</h4>
-                <h5 style={{marginTop:0,}}><span style={{display: window.innerWidth > 768 ? 'inline' : 'block', paddingRight: '5px'}}><Icon style={{marginRight:0}} name='dollar'/>Top 100 CDPs Debt: {this.truncateNumber(this.state.top100Debt)} DAI ({(this.state.top100Debt/this.state.circulatingDai*100).toFixed(2)}%)</span> <Icon style={{marginRight:0}} name='ethereum'/>Top 100 CDPs Collateral: {this.truncateNumber(this.state.top100Collateral)} PETH ({(this.state.top100Collateral/this.state.lockedPeth*100).toFixed(2)}%)</h5>
-                <button style={{display: 'block', background: 'none', border:'none', padding:0, textDecoration:'underline', color:'#FFF', cursor:'pointer'}} value='ShowDebt' onClick={this.filterTable}>{this.state.filtered ? "Show All CDPs" : "Hide Debt-Free CDPs"}</button>
+                <Grid columns={2} stackable>
+                    <Grid.Column>
+                        <h4 ><Icon name='globe' />Open CDPs (Total {numberCDPs})</h4>
+                    </Grid.Column>
+                    <Grid.Column textAlign={ window.innerWidth < 768 ? 'left' : 'right'}>
+                    <h5 style={{marginTop:0,}}><span style={{display: window.innerWidth > 768 ? 'inline' : 'block', paddingRight: '5px'}}><Icon style={{marginRight:0}} name='dollar'/>{`Top ${this.state.pageSize} CDPs Debt: `}{this.truncateNumber(this.state.top100Debt)} DAI ({(this.state.top100Debt/this.state.circulatingDai*100).toFixed(2)}%)</span> <Icon style={{marginRight:0}} name='ethereum'/>{`Top ${this.state.pageSize} CDPs Collateral`}: {this.truncateNumber(this.state.top100Collateral)} PETH ({(this.state.top100Collateral/this.state.lockedPeth*100).toFixed(2)}%)</h5>
+                    </Grid.Column>
+                    <Grid.Column style={{paddingTop:0}}>
+                    <button style={{ background: 'none', border:'none', padding:0, textDecoration:'underline', color:'#FFF', cursor:'pointer'}} value='ShowDebt' onClick={this.filterTable}>{this.state.filtered ? "Show All CDPs" : "Hide Debt-Free CDPs"}</button>
+                    </Grid.Column>
+                    
+                </Grid>
                 <hr style={{opacity:'0.7'}}/>
               <ReactTable
                 data = {processedCdps}
@@ -128,10 +138,12 @@ export default class AllCdps extends Component{
                             {
                                 Header: <span>CDP ID <Icon inverted name='sort'/></span>,
                                 accessor: 'cdpId',
+                                // Cell: row => <div style={{textAlign:'justify'}}>{row.value}</div>
                             },
                             {
                                 Header: <span>Owner <Icon inverted name='sort'/></span>,
-                                accessor: 'account'
+                                accessor: 'account',
+                                Cell: row => <div style={{textAlign:'left'}}>{row.value}</div>
                             },
                             {
                                 Header: <span>Debt (DAI) <Icon inverted name='sort'/></span>,
@@ -186,6 +198,7 @@ export default class AllCdps extends Component{
                 style={{color:'#FFF', textAlign:'center', }}
                 previousText={<p style={{color:'#FFF', }}>Previous</p>}
                 nextText={<p style={{color:'#FFF',}}>Next</p>}
+                onPageSizeChange = {(pageSize) => this.processCdps(pageSize)}
               />
             </div>
           )
