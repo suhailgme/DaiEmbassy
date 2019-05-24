5ed2f8a096c6ee940b49b25e39890bccb77ecf87
SuhailG <zonyc17@msn.com>
2019-05-24 04:30:15 -0600

    Remove left over gifs and js files

--- maker.js
+++
@@ -1,9 +0,0 @@
-const Maker = require('@makerdao/dai')
-const maker = Maker.create('test');
-
-(async () =>{
-    await maker.authenticate()
-//     const priceService = maker.service('price');
-//     const newCdp = await maker.openCdp()
-//     console.log(newCdp)
-})()
