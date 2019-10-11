777610d00fbd4227c7e57227f14f3ff4c593344a
SuhailG <zonyc17@msn.com>
2019-10-11 15:59:15 -0600

    Change locked ETH/PETH to change value on hover instead of popup

--- src/components/SystemStatusWidget.js
+++ src/components/SystemStatusWidget.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import { Grid, Statistic, Container, Form, Popup } from 'semantic-ui-react'
 
-export default class SystemStatusWidget extends Component{
-    
+export default class SystemStatusWidget extends Component {
+
     state = {
-        systemStatus:this.props.systemStatus,
+        systemStatus: this.props.systemStatus,
         avgDebtString: "Avg. CDP Debt",
         medDebtString: "Median CDP Debt",
         avgCollateralString: "Avg. CDP Collateral",
         medCollateralString: "Median CDP Collateral",
         displayDaiAvg: 1,
-        displayCollaterAvg: 1
+        displayCollaterAvg: 1,
+        displayLockedEth: 1
     }
 
 
@@ -20,8 +21,8 @@ export default class SystemStatusWidget extends Component{
     //       this.setStateStrings();
     //     }
     //   }
-      
-    componentDidMount(){
+
+    componentDidMount() {
         this.setState({
             displayDai: this.state.avgDebtString,
             displayPeth: this.state.avgCollateralString,
@@ -31,21 +32,21 @@ export default class SystemStatusWidget extends Component{
         // console.log(this.state)
     }
 
-    static getDerivedStateFromProps(nextProps, prevState){
+    static getDerivedStateFromProps(nextProps, prevState) {
         // console.log('system status: ', nextProps.systemStatus)
         // console.log('systemStatus: ', prevState)
 
         return {
             systemStatus: nextProps.systemStatus,
-            }
         }
+    }
 
     numberWithCommas = (number) => {
         if (number)
             return number.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
         return null
-      }
-    
+    }
+
 
 
     onMouseOverDai(e) {
@@ -74,6 +75,20 @@ export default class SystemStatusWidget extends Component{
         this.setState({
             displayCollaterAvg: !this.state.displayCollaterAvg,
             displayPeth: this.state.avgCollateralString
+
+        })
+    }
+
+    onMouseOverLockedCollateral(e) {
+        this.setState({
+            displayLockedEth: !this.state.displayLockedEth,
+
+        })
+    }
+
+    onMouseoutLockedCollateral(e) {
+        this.setState({
+            displayLockedEth: !this.state.displayLockedEth,
 
         })
     }
@@ -123,30 +138,30 @@ export default class SystemStatusWidget extends Component{
     //     return lockedEthSupply
     // }
 
-    render(){
+    render() {
         const systemStatus = this.state.systemStatus
         return (
             <div>
-                <Container textAlign='center' style={{paddingBottom:'10px'}}>
+                <Container textAlign='center' style={{ paddingBottom: '10px' }}>
                     <Statistic.Group size='mini' widths={2}>
-                    <Statistic color='red' inverted  label='ETH/USD' value={systemStatus.ethPrice}/>
-                    <Statistic inverted>
-                        <Statistic.Value style={{color:'#E6BB48'}}>{systemStatus.daiPrice.dai_usd_price}</Statistic.Value>
-                        <Popup inverted content='DAI price provided by daiprice.info' trigger={<Statistic.Label>DAI/USD</Statistic.Label>}/>
-                    </Statistic>
-                </Statistic.Group>
+                        <Statistic color='red' inverted label='ETH/USD' value={systemStatus.ethPrice} />
+                        <Statistic inverted>
+                            <Statistic.Value style={{ color: '#E6BB48' }}>{systemStatus.daiPrice.dai_usd_price}</Statistic.Value>
+                            <Popup inverted content='DAI price provided by daiprice.info' trigger={<Statistic.Label>DAI/USD</Statistic.Label>} />
+                        </Statistic>
+                    </Statistic.Group>
                 </Container>
                 <Form>
-                <Grid columns='equal' padded='vertically'>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>MKR Price</Grid.Column>
-                        <Grid.Column textAlign='right' style={{color:'#6abf69'}}>{systemStatus.mkrPrice} USD</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left' style={{whiteSpace:'nowrap' }}>ETH Supply Locked</Grid.Column>
-                        <Grid.Column textAlign='right' style={{color:'#6abf69'}}>{systemStatus.lockedEthSupply} %</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
+                    <Grid columns='equal' padded='vertically'>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px' }}>
+                            <Grid.Column textAlign='left'>MKR Price</Grid.Column>
+                            <Grid.Column textAlign='right' style={{ color: '#6abf69' }}>{systemStatus.mkrPrice} USD</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px' }}>
+                            <Grid.Column textAlign='left' style={{ whiteSpace: 'nowrap' }}>ETH Supply Locked</Grid.Column>
+                            <Grid.Column textAlign='right' style={{ color: '#6abf69' }}>{systemStatus.lockedEthSupply} %</Grid.Column>
+                        </Grid.Row>
+                        {/* <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
                         <Grid.Column textAlign='left'>
                         <Popup inverted trigger={<a href="#" style={{textDecoration:'underline', color:'inherit', cursor:'help'}}> Locked ETH</a>} on='hover'>
                             <Popup.Header>Locked PETH{this.state.cdpId}</Popup.Header>
@@ -156,40 +171,44 @@ export default class SystemStatusWidget extends Component{
                             </Popup> 
                         </Grid.Column>
                         <Grid.Column textAlign='right' style={{color:'#FF695E', whiteSpace:'nowrap'}}> {`${this.numberWithCommas(systemStatus.lockedEth)}`}</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>PETH/ETH</Grid.Column>
-                        <Grid.Column textAlign='right'>{systemStatus.pethWethRatio}</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>Circ. DAI</Grid.Column>
-                        <Grid.Column textAlign='right' style={{color:'#FF695E', whiteSpace:'nowrap'}}>{`${this.numberWithCommas(systemStatus.circulatingDai)}`}</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px', cursor:'help'}} onMouseEnter={this.onMouseOverDai.bind(this)} onMouseLeave={this.onMouseoutDai.bind(this)} id='dai'>
-                        <Grid.Column textAlign='left' style={{whiteSpace:'nowrap', textDecoration:'underline', color:'inherit', }}>{this.state.displayDai}</Grid.Column>
-                        <Grid.Column textAlign='right' style={{ color: this.state.displayDaiAvg ?'#6abf69' : '#FF695E'}} >{this.state.displayDaiAvg ? `${this.numberWithCommas(systemStatus.avgDebt)} DAI` : `${this.numberWithCommas(systemStatus.medianDebt)} DAI`}</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px', cursor:'help'}} onMouseEnter={this.onMouseOverPeth.bind(this)} onMouseLeave={this.onMouseoutPeth.bind(this)} id='peth'>
-                        <Grid.Column textAlign='left' style={{whiteSpace:'nowrap', textDecoration:'underline'}}>{this.state.displayPeth}</Grid.Column>
-                        <Grid.Column textAlign='right' style={{color: this.state.displayCollaterAvg ?'#6abf69' : '#FF695E'}}>{this.state.displayCollaterAvg ? `${this.numberWithCommas(systemStatus.avgCollateral)} PETH` : `${parseFloat(systemStatus.medianCollateral).toFixed(2)} PETH`}</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>Collateralization</Grid.Column>
-                        <Grid.Column textAlign='right' style={{color:'#6abf69'}}>{systemStatus.systemCollateralization} %</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>Stability Fee</Grid.Column>
-                        <Grid.Column textAlign='right' style={{color:'#FF695E'}}>{systemStatus.stabilityFee} %</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>Total CDPs</Grid.Column>
-                        <Grid.Column textAlign='right'>{systemStatus.totalCdps}</Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row style={{paddingTop:'0',paddingBottom:'5px'}}>
-                        <Grid.Column textAlign='left'>Updated</Grid.Column>
-                        <Grid.Column textAlign='right'>{Date().toString().slice(4,-47) + Date().toString().slice(16,-37)}</Grid.Column>
-                    </Grid.Row>
-                </Grid>
+                    </Grid.Row> */}
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px', cursor: 'help' }} onMouseEnter={this.onMouseOverLockedCollateral.bind(this)} onMouseLeave={this.onMouseoutLockedCollateral.bind(this)} id='lockedCollateral'>
+                            <Grid.Column textAlign='left' style={{ whiteSpace: 'nowrap', textDecoration: 'underline' }}>{this.state.displayLockedEth ? 'Locked ETH' : 'Locked PETH'}</Grid.Column>
+                            <Grid.Column textAlign='right' style={{ whiteSpace: 'nowrap', color: this.state.displayLockedEth ? '#FF695E' : '#6abf69' }}>{this.state.displayLockedEth ? `${this.numberWithCommas(systemStatus.lockedEth)}` : `${this.numberWithCommas(systemStatus.lockedPeth)}`}</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px' }}>
+                            <Grid.Column textAlign='left' style={{whiteSpace: 'nowrap'}}>PETH/ETH Ratio</Grid.Column>
+                            <Grid.Column textAlign='right'>{systemStatus.pethWethRatio}</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px' }}>
+                            <Grid.Column textAlign='left'>Circ. DAI</Grid.Column>
+                            <Grid.Column textAlign='right' style={{ color: '#FF695E', whiteSpace: 'nowrap' }}>{`${this.numberWithCommas(systemStatus.circulatingDai)}`}</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px', cursor: 'help' }} onMouseEnter={this.onMouseOverDai.bind(this)} onMouseLeave={this.onMouseoutDai.bind(this)} id='dai'>
+                            <Grid.Column textAlign='left' style={{ whiteSpace: 'nowrap', textDecoration: 'underline', color: 'inherit', }}>{this.state.displayDai}</Grid.Column>
+                            <Grid.Column textAlign='right' style={{ color: this.state.displayDaiAvg ? '#6abf69' : '#FF695E' }} >{this.state.displayDaiAvg ? `${this.numberWithCommas(systemStatus.avgDebt)} DAI` : `${this.numberWithCommas(systemStatus.medianDebt)} DAI`}</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px', cursor: 'help' }} onMouseEnter={this.onMouseOverPeth.bind(this)} onMouseLeave={this.onMouseoutPeth.bind(this)} id='peth'>
+                            <Grid.Column textAlign='left' style={{ whiteSpace: 'nowrap', textDecoration: 'underline' }}>{this.state.displayPeth}</Grid.Column>
+                            <Grid.Column textAlign='right' style={{ color: this.state.displayCollaterAvg ? '#6abf69' : '#FF695E' }}>{this.state.displayCollaterAvg ? `${this.numberWithCommas(systemStatus.avgCollateral)} PETH` : `${parseFloat(systemStatus.medianCollateral).toFixed(2)} PETH`}</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px' }}>
+                            <Grid.Column textAlign='left'>Collateralization</Grid.Column>
+                            <Grid.Column textAlign='right' style={{ color: '#6abf69' }}>{systemStatus.systemCollateralization} %</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px' }}>
+                            <Grid.Column textAlign='left'>Stability Fee</Grid.Column>
+                            <Grid.Column textAlign='right' style={{ color: '#FF695E' }}>{systemStatus.stabilityFee} %</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px' }}>
+                            <Grid.Column textAlign='left'>Total CDPs</Grid.Column>
+                            <Grid.Column textAlign='right'>{systemStatus.totalCdps}</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row style={{ paddingTop: '0', paddingBottom: '5px' }}>
+                            <Grid.Column textAlign='left'>Updated</Grid.Column>
+                            <Grid.Column textAlign='right'>{Date().toString().slice(4, -47) + Date().toString().slice(16, -37)}</Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </Form>
             </div>
         )
