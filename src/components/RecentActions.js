1e0b0532a41e2acb4eecb303bdb7136df09fd5d8
SuhailG <zonyc17@msn.com>
2019-10-11 15:58:32 -0600

    Add number of actions and age to CDP's recent actions table

--- src/components/RecentActions.js
+++ src/components/RecentActions.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
-import {  Loader, Icon, Grid } from 'semantic-ui-react'
+import { Loader, Icon, Button } from 'semantic-ui-react'
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 const Humanize = require('humanize-plus')
 const axios = require('axios')
 
 
-class RecentActions extends Component{
-    state = { 
+class RecentActions extends Component {
+    state = {
         // cdp: this.props.cdps.find((cdp) =>{
         // return cdp.cdpId === this.props.cdpId
         // }),
         // cdpId: this.props.cdpId
+        pethWethRatio: this.props.systemStatus.pethWethRatio
     }
 
     async componentDidMount() {
@@ -26,41 +27,41 @@ class RecentActions extends Component{
     }
     async componentDidUpdate(prevProps) {
         if (this.props.cdpId && this.props.cdpId !== prevProps.cdpId) {
-                const cdpRes = await axios.get(`https://api.daiembassy.com/cdp?id=${this.props.cdpId}`)
-                const recentActions = cdpRes.data.actions
-                this.setState({ recentActions })
+            const cdpRes = await axios.get(`https://api.daiembassy.com/cdp?id=${this.props.cdpId}`)
+            const recentActions = cdpRes.data.actions
+            this.setState({ recentActions })
         }
-      }
-    
-    static getDerivedStateFromProps(nextProps, prevState){
-        if(nextProps.cdpId && nextProps.cdpId !==prevState.cdpId){
-                return {cdpId: nextProps.cdpId, recentActions:null }
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.cdpId && nextProps.cdpId !== prevState.cdpId) {
+            return { cdpId: nextProps.cdpId, recentActions: null }
         }
         return null
 
     }
 
 
-    getActions = () =>{
+    getActions = () => {
         let recentActions = []
         const actions = this.state.recentActions
-        if(new Date(actions[0].time) < new Date(actions[actions.length-1].time)){
+        if (new Date(actions[0].time) < new Date(actions[actions.length - 1].time)) {
             actions.reverse()
         }
-        
+
         // console.log("actions:", actions)
-        
+
         // console.log('actions: ', actions)
         actions.forEach((action, index) => {
             recentActions.push(
                 {
                     time: action.time,//new Date(action.time).toString().slice(0,-37),
-                    act: `${action.act} ${action.act === 'OPEN' || action.act === 'GIVE' ? '' : action.act == 'DRAW' || action.act == 'WIPE' ? `${this.numberWithCommas(action.arg)} DAI` : action.act == 'LOCK' || action.act == 'FREE' ? `${this.numberWithCommas(action.arg)} PETH`: ''}`,
-                    tx: <a target="_blank" href={`https://etherscan.io/tx/${action.tx}`} style={{textDecoration:'underline', color:'inherit'}}>{this.truncateTx(action.tx)}</a>,
-                    price: `$${this.numberWithCommas(action.pip)}`,
+                    act: `${action.act} ${action.act === 'OPEN' || action.act === 'GIVE' ? '' : action.act == 'DRAW' || action.act == 'WIPE' ? `${this.numberWithCommas(action.arg)} DAI` : action.act == 'LOCK' || action.act == 'FREE' ? `${this.numberWithCommas(action.arg)} PETH` : ''}`,
+                    tx: <a target="_blank" href={`https://etherscan.io/tx/${action.tx}`} style={{ textDecoration: 'underline', color: 'inherit' }}>{this.truncateTx(action.tx)}</a>,
+                    price: `$${this.numberWithCommas(action.pip, 2)}`,
                     debt: `${this.numberWithCommas(action.art)} DAI`,
                     collateral: `${this.numberWithCommas(action.ink)} PETH`,
-                    ratio: `${this.numberWithCommas(action.ratio)}`,
+                    ratio: action.act === 'BITE' ? String(((actions[index + 1].ink * action.pip * this.state.pethWethRatio) / actions[index + 1].art) * 100) : action.ratio,
                     id: index
                 })
         })
@@ -68,23 +69,25 @@ class RecentActions extends Component{
     }
 
     truncateTx = (tx) => {
-        return `${tx.slice(0,6)}...${tx.slice(-6)}` 
+        return `${tx.slice(0, 6)}...${tx.slice(-6)}`
     }
 
-    numberWithCommas = (number) => {
-        return Humanize.formatNumber(number,2)
+    numberWithCommas = (number, commas = 3) => {
+        return Humanize.formatNumber(number, commas)
         // return number.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-      }
+    }
 
 
-    render(){
+    render() {
         const loaded = this.state.recentActions
-        if(loaded){
-        const actions = this.getActions()
-        return (
-            <div style={{color:'#FFF', borderRadius:'5px', border: '2px solid #38414B',
-            backgroundColor:'#273340', paddingTop:'10px', paddingLeft:'5px'}}>
-                {/* <Grid columns={2}>
+        if (loaded) {
+            const actions = this.getActions()
+            return (
+                <div style={{
+                    color: '#FFF', borderRadius: '5px', border: '2px solid #38414B',
+                    backgroundColor: '#273340', paddingTop: '10px', paddingLeft: '5px'
+                }}>
+                    {/* <Grid columns={2}>
                     <Grid.Column>
                         <h4 ><Icon name='target'/>CDP {this.state.cdpId} Actions</h4>
                     </Grid.Column>
@@ -93,17 +96,24 @@ class RecentActions extends Component{
                     <span style={{cursor:'pointer'}}onClick={this.props.clearCdp}><Icon name='close'/></span>
                     </Grid.Column>
                 </Grid> */}
-                 <h4><Icon name='target'/>CDP {this.state.cdpId} Actions</h4>
-                <button style={{ background: 'none', border:'none', padding:0, textDecoration:'underline', color:'#FFF', cursor:'pointer'}} onClick={this.props.clearCdp}>Clear CDP</button>
-                <hr style={{opacity:'0.7'}}/>
-              <ReactTable
-                data = {actions}
-                columns= {[
-                    
+                    <div style={{ display: 'flex', flexDirection: window.innerWidth > 768 ? 'row' : 'column' }}>
+                        <h4 style={{marginBottom:0, paddingRight: '10px', paddingBottom:'14px'}}><Icon name='target' />CDP {this.state.cdpId}</h4>
+                        <h4 style={{marginTop:0, marginBottom:0, paddingBottom:'14px', paddingRight: '10px'}}><Icon name='sitemap' />Total Actions: {actions.length}</h4>
+                        <h4 style={{marginTop:0, paddingBottom:'14px'}}><Icon name='clock' />Age: {this.numberWithCommas((Math.floor(Date.parse(new Date()) - Date.parse(actions[actions.length-1].time)) / 86400000), 0)} Days</h4>
+
+                    </div>
+
+                    {/* <h4>Interactions: {actions.length}</h4> */}
+                    <Button size='tiny' compact onClick={this.props.clearCdp}>Clear CDP</Button>
+                    <hr style={{ opacity: '0.7' }} />
+                    <ReactTable
+                        data={actions}
+                        columns={[
+
                             {
                                 id: 'date',
                                 Header: 'Date (local)',
-                                accessor: data => new Date(data.time).toString().slice(0,-37)
+                                accessor: data => new Date(data.time).toString().slice(0, -34)
                             },
                             {
                                 Header: 'Action',
@@ -120,37 +130,39 @@ class RecentActions extends Component{
                             {
                                 id: 'ratio',
                                 Header: 'Collateralization Ratio',
-                                accessor: data =>data.ratio,
-                                Cell: props => <p style={{color: props.value == 0.00 ? '#FFF' : props.value < 170 ? '#FF695E' : props.value < 200 ? '#EFBC72' : '#FFF' }}>{`${props.value} %`}</p>,
+                                accessor: 'ratio',
+                                Cell: props => !props.value || props.value === 'Infinity' ? <p>0.00 %</p> : <p style={{ color: props.value == 0.00 ? '#FFF' : props.value < 170 ? '#FF695E' : props.value < 200 ? '#EFBC72' : '#FFF' }}>{`${this.numberWithCommas(props.value, 2)} %`}</p>,
 
                             },
                             {
                                 Header: "ETH/USD",
                                 accessor: "price"
-                            },  
+                            },
                             {
                                 Header: "Tx Hash",
                                 accessor: "tx"
-                            },  
-                ]}
-                className="-striped -highlight"
-                style={{color:'#FFF', textAlign:'center', height:'350px',}}
-                showPagination = {false}
-                defaultPageSize = {actions.length}
-                sortable = {false}
-              />
-            </div>
-          )
-        }else{
-            return (
-                <div style={{color:'#FFF', borderRadius:'5px', border: '2px solid #38414B',
-                backgroundColor:'#273340', paddingTop:'10px', paddingLeft:'5px', height:'406px'}}>
-                <h4><Icon name='target'/>Loading CDP Actions</h4>
-                <hr style={{opacity:'0.7'}}/>
-                <Loader active inverted inline='centered'/>
-                
+                            },
+                        ]}
+                        className="-striped -highlight"
+                        style={{ color: '#FFF', textAlign: 'center', height: '350px', }}
+                        showPagination={false}
+                        defaultPageSize={actions.length}
+                        sortable={false}
+                    />
                 </div>
-                )
+            )
+        } else {
+            return (
+                <div style={{
+                    color: '#FFF', borderRadius: '5px', border: '2px solid #38414B',
+                    backgroundColor: '#273340', paddingTop: '10px', paddingLeft: '5px', height: '406px'
+                }}>
+                    <h4><Icon name='target' />Loading CDP Actions</h4>
+                    <hr style={{ opacity: '0.7' }} />
+                    <Loader active inverted inline='centered' />
+
+                </div>
+            )
         }
         // return(
         //     <Segment style={{paddingTop:'10px', backgroundColor:'#232D39', borderRadius:'5px', border: '2px solid #38414B',maxHeight:384.5, overflow:'auto'}}>
